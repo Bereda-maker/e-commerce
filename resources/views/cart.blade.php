72c9b2bbe90870ecd346f8547382df4129b86f0e
Bereda <beriwako56@gmail.com>
2026-09-24 03:26:28 -0700

    Redesign: resources/views/cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
-    <h1 class="text-2xl font-semibold mb-6">Your cart</h1>
-    @livewire('cart-component')
+    <div class="max-w-5xl mx-auto px-4 sm:px-6 py-10">
+        <h1 class="text-2xl font-extrabold text-ink-900 mb-8">Your cart</h1>
+        @livewire('cart-component')
+    </div>
 </x-layout>
